a7ffb1dfacc1f1d9f4ff8e4477561625868f9038
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-04 23:09:48 +0600

    Add files via upload

---
+++ enter 3 names in srting.py
@@ -0,0 +1,13 @@
+"""write to ask the user 
+to enter names of their 3 
+favorite movies & store them in a list"""
+
+a=str(input("enter 1st movie name= "))
+b=str(input("enter 2nd movie name= "))
+c=str(input("enter 3rd movie name= "))
+
+d=[]
+d.append(a)
+d.append(b)
+d.append(c)
+print("all movie in a list =",d)
